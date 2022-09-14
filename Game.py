375953828646b3b38007cefc03b0a4c9582ab2a1
Said <56594350+okaal@users.noreply.github.com>
2022-09-14 11:51:32 +0200

    Delete Game.py

--- Game.py
+++
@@ -1,7 +0,0 @@
-farge = input("Skriv inn en farge: ")
-flertall_substantiv = input("Skriv et substantiv i flertall: ")
-kjendis = input("Skriv inn en kjendis: ")
-
-print("Roser er " + farge)
-print(flertall_substantiv + " er blå ")
-print("Jeg elsker " + kjendis)
