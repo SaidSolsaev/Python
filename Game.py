f5396d28426ee60538116a81931bbea66b9d95ef
Said <56594350+okaal@users.noreply.github.com>
2022-09-14 11:51:21 +0200

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,7 @@
+farge = input("Skriv inn en farge: ")
+flertall_substantiv = input("Skriv et substantiv i flertall: ")
+kjendis = input("Skriv inn en kjendis: ")
+
+print("Roser er " + farge)
+print(flertall_substantiv + " er blå ")
+print("Jeg elsker " + kjendis)
